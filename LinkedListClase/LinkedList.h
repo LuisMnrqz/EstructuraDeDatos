e08659374597435969f155ad746c7130a6a81557
Luis Manríquez <56151652+LuisMnrqz@users.noreply.github.com>
2020-03-20 13:41:29 -0600

    p

--- LinkedListClase/LinkedList.h
+++ LinkedListClase/LinkedList.h
@@ -273,23 +273,7 @@ bool LinkedList<T>::change(int pos1, int pos2)
 template <class T>
 void LinkedList<T>::reverse()
 {
-    Node<T> *nodo = head;
-
-    if (nodo == NULL) 
-    {
-        return NULL;
-    } 
-
-    if (node->next == NULL) 
-    { 
-        head = node; 
-        return node; 
-    } 
-
-    Node* node1 = reverse(node->next); 
-    node1->next = node; 
-    node->next = NULL; 
-    return node; 
+    
 }
 
 template <class T>
