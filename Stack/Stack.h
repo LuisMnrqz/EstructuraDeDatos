89b7c3cb449e1d16f3fc47ef49257668ebf8ed8e
Luis Manríquez <56151652+LuisMnrqz@users.noreply.github.com>
2020-03-31 08:58:50 -0600

    stack

---
+++ Stack/Stack.h
@@ -0,0 +1,70 @@
+#include "Node.h"
+
+template <class T>
+class stack {
+    public:
+        stack();
+        ~stack();
+        void push(T data);
+        void pop();
+        T top();
+        bool empty();
+        int size();
+    private:
+        Node<T> *tope;
+        int tam;
+};
+
+template <class T>
+stack<T>::stack()
+{
+    tope = nullptr;
+    tam = 0;
+}
+
+template <class T>
+stack<T>::~stack()
+{
+    Node<T> *curr = tope;
+    
+    while (tope != nullptr)
+    {
+        tope = tope->getNext();
+        delete curr;
+        curr = tope;
+    }
+}
+
+template <class T>
+bool stack<T>::empty()
+{
+    return tope == nullptr;
+}
+
+template <class T>
+int stack<T>::size()
+{
+    return tam;
+}
+
+template <class T>
+T stack<T>::top()
+{
+    return tope->getData();
+}
+
+template <class T>
+void stack<T>::push(T data)
+{
+    tope = new Node<T>(data, tope);
+    tam++;
+}
+
+template <class T>
+void stack<T>::pop()
+{
+    Node<T> *curr = tope;
+    tope = tope->getNext();
+    delete curr;
+    tam--;
+}
